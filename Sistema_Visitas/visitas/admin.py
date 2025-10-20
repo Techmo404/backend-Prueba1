c0c832635d9208069b4cd53c493d019399e150ce
benjamin miranda <benjamin.miranda27@inacapmail.cl>
2025-10-20 16:51:42 -0300

    feat: preparacion prueba

--- Sistema_Visitas/visitas/admin.py
+++ Sistema_Visitas/visitas/admin.py
@@ -3,6 +3,18 @@ from django.contrib import admin
 from django.contrib import admin
 from .models import Visita
 #permite registrar el modelo visita en el panel de administracion
-admin.site.register(Visita)
+# admin.site.register(Visita)
 
 # Register your models here.
+@admin.register(Visita)
+class VisitaAdmin(admin.ModelAdmin):
+    list_display = ('nombre','rut','fecha','motivo_visita')
+    list_filter = ('fecha',)
+    search_fields = ('nombre','rut')
+    list_editable = ('motivo_visita',)
+    ordering = ('fecha',)
+    readonly_fields = ('hora_entrada','hora_salida')
+
+    fieldsets = (
+        ('Persona', {'fields': ('nombre', 'rut')})
+                 ,)
