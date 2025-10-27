f6019a631837e23b95acdc515735c342ba1098c2
benjamin miranda <benjamin.miranda27@inacapmail.cl>
2025-10-27 17:19:38 -0300

    Update views.py

--- visitas/views.py
+++ visitas/views.py
@@ -5,7 +5,7 @@ from datetime import date
 # Obtiene los datos de visitas y lo renderiza en la variable visitas
 def lista_visitas(request):
     visitas = Visita.objects.all() 
-    return render(request, 'visitas/lista_visitas.html', {'visitas': visitas})
+    return render(request, 'lista_visitas.html', {'visitas': visitas})
 #Crea una instancia del formulario
 def nueva_visita(request):
     if request.method == 'POST':
@@ -15,7 +15,7 @@ def nueva_visita(request):
             return redirect('lista_visitas')
     else:
         form = VisitaForm()
-    return render(request, 'visitas/nueva_visita.html', {'form': form})
+    return render(request, 'nueva_visita.html', {'form': form})
 #Busca si el id es valido y elimina una visita
 def eliminar_visita(request, visita_id):
     visita = get_object_or_404(Visita, id=visita_id)
