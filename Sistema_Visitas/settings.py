ca9bbd8b4a4c0fd2afcce2b4173fa7c7fd61692a
benjamin miranda <benjamin.miranda27@inacapmail.cl>
2025-10-27 16:54:27 -0300

    feat:actualization

--- Sistema_Visitas/settings.py
+++ Sistema_Visitas/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -32,9 +36,7 @@ ALLOWED_HOSTS = [
 
 STATIC_URL = '/static'
 STATIC_ROOT = BASE_DIR/ 'public'
-STATICFILES_DIRS= [
-    BASE_DIR / 'static'
-]
+
 
 
 
@@ -85,8 +87,12 @@ WSGI_APPLICATION = 'Sistema_Visitas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DBNAME"),
+        "USER":os.getenv("USER"),
+        "PASSWORD": os.getenv("PASSWORD"),
+        "HOST": os.getenv("HOST"),
+        "PORT": os.getenv("PORT"),
     }
 }
 
