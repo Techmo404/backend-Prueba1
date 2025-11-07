2d8e4d66c3234503bd61bda8034d1be32d8eaf2f
benjamin miranda <benjamin.miranda27@inacapmail.cl>
2025-11-07 16:03:25 -0300

    Update settings.py

--- Sistema_Visitas/settings.py
+++ Sistema_Visitas/settings.py
@@ -91,7 +91,7 @@ DATABASES = {
         "USER":os.getenv("USER"),
         "PASSWORD": os.getenv("PASSWORD"),
         "HOST": os.getenv("HOST"),
-        "PORT": os.getenv("PORT"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
